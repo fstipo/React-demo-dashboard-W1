d0505c1b71d95e99b1773f6ad364958e544e3986
franko <fsitpanov@gmail.com>
2023-02-23 11:46:00 +0100

    toast hideProgressNar

--- src/layouts/components/Main.jsx
+++ src/layouts/components/Main.jsx
@@ -9,8 +9,8 @@ const Main = () => {
         <>
             <ToastContainer
                 position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
+                autoClose={3000}
+                hideProgressBar={true}
                 newestOnTop={false}
                 closeOnClick
                 rtl={false}
